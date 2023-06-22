422f4244f0f6a7bc9b37a04e39f2ffc9fac30582
Ronald Michiels <ronald.michiels@inetum-realdolmen.world>
2023-06-22 12:02:15 +0200

    Implemented WCF Service Library that calculates taxes

---
+++ WCFMyServiceLib/MyWCFLibService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFMyServiceLib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class MyWCFLibService : IMyWCFLibService
+    {
+        public string GetData(int value)
+        {
+            float taxRate = 0.21f;
+            var tax = value * taxRate;
+            var total = value + tax;
+            return string.Format("You have to pay a total of ${0}, of which ${1} in taxes.", total, tax);
+        }
+    }
+}
